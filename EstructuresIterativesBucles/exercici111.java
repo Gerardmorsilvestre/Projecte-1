bee25b9720ce31f55c2229052bd62f5926480648
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-11-08 15:55:56 +0100

    Update exercici111.java

--- EstructuresIterativesBucles/exercici111.java
+++ EstructuresIterativesBucles/exercici111.java
@@ -2,7 +2,16 @@ package EstructuresIterativesBucles;
 Import.java.util.Scanner;
 public class exercici111 {
     public static void main (String[]args){
+        System.out.println("Digam dos numeros i et dire la potiencia d'aqulls");
+        Scanner as = new Scanner(System.in)
+        System.out.println("Digam un valor A");
+        System.out.println("Digam un valor B");
+        int a = as.nextInt();
+        int b = as.nextInt();
+        
+        
         
     
 }
+
 
