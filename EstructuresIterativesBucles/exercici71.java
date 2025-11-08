fccf97ef7881d96e999883fca603820e479ef5fc
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-11-08 14:05:28 +0100

    Enhance exercici71 to count positive and negative numbers
    Added counters for positive and negative numbers.

--- EstructuresIterativesBucles/exercici71.java
+++ EstructuresIterativesBucles/exercici71.java
@@ -6,15 +6,23 @@ public class exercici71{
         System.out.println("Digues numeros no nuls fins que posis el numero 0 on despres et fare un recompte de tots els que has posat tant negatius com positius");
         int quntnum;
         int cont = 0;
+        int contmax = 0;
+        int contless = 0;
 
         while (quntnum != 0){
             if (quntnum > 0){
                 cont++;
+                contmax++;
             } if (quntnum < 0){
                 cont++;
+                contless++;
             }
         } 
+        System.out.println("Numero totals introduits " + cont);
+        System.out.println("Numero positius introduits " + contmax);
+        System.out.println("Numeros negatius introduits " + contless);
         
     }
 
 }
+
