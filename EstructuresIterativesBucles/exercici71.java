66a52fa26c8f0ac8654ec3865a787554b897cf65
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-11-08 12:51:06 +0100

    g

--- EstructuresIterativesBucles/exercici71.java
+++ EstructuresIterativesBucles/exercici71.java
@@ -1,3 +1,13 @@
 package EstructuresIterativesBucles;
+Import java.util.Scanner;
+public class exercici71{
+    public static void main(String[]args){
+        Scanner as = new Scanner(System.in);
+        System.out.println("Digues numeros no nuls fins que posis el numero 0 on despres et fare un recompte de tots els que has posat tant negatius com positius");
+        int quntnum;
+        int cont = 0;
 
-public class exercici71{}
+        for(int i = 0; i !=0; i+=1 )
+
+    }
+}
