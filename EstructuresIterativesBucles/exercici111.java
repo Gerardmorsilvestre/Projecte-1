9586dd7e3cfe903842937c410015c2ed21f49c47
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-11-08 16:03:49 +0100

    Correct power calculation in exercici111.java
    Fix variable naming and add power calculation logic.

--- EstructuresIterativesBucles/exercici111.java
+++ EstructuresIterativesBucles/exercici111.java
@@ -9,9 +9,15 @@ public class exercici111 {
         int a = as.nextInt();
         int b = as.nextInt();
         
-        
-        
-    
+        int resultat = 1; // Iniciem en resultat com a 1 (element neutre de la multiplicació)
+        // Bucle for: S'ejecuta B vegades per multiplicar A per si mateix
+        for (int i=1; i <=B; i++;){
+            resultat *=A //En cada interacció res = res*A
+        }
+        System.out.println("Resultat: " + resultat)
+        as.close();
+}   
 }
+
 
 
